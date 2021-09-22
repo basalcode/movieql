5739b32d4db437b8bb0d55e786dc12fdd2165858
basalcode <basalcode@gmail.com>
2021-09-22 20:52:06 +0900

    Create more queries and resolvers

--- graphql/db.js
+++ graphql/db.js
@@ -1,26 +1,51 @@
-export const people = [
+let movies = [
     {
-        id: "0",
-        name: "Alex",
-        age: 20,
-        gender: "male"
+        id: 0,
+        name: "Star wars - The new one",
+        score: 1
     },
     {
-        id: "1",
-        name: "Jhone",
-        age: 22,
-        gender: "male"
+        id: 1,
+        name: "Avengers - The new one",
+        score: 8
     },
     {
-        id: "2",
-        name: "Jennie",
-        age: 21,
-        gender: "female"
+        id: 2,
+        name: "The Godfatrher I",
+        score: 99
+    },
+    {
+        id: 3,
+        name: "Logan",
+        score: 2
     },
 ];
+
+export const getMovies = () => movies;
 
 export const getById = id => {
     const filteredPeople = people.filter(person => person.id === String(id));
 
     return filteredPeople[0];
+}
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+
+    return newMovie;
 }
